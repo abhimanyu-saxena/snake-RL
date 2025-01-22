57734f4d1b81b3b5f7d54b771083caa421bc1cde
Abhimanyu Saxena <abhi.manyu1225@gmail.com>
2025-01-21 20:51:16 -0600

    Added playback using trained model

--- agent.py
+++ agent.py
@@ -6,6 +6,7 @@ from env import Direction, Point
 from collections import deque
 from model import QTrainer, DQN
 from utils import plot
+import argparse
 
 class Agent:
     def __init__(self):
@@ -69,8 +70,11 @@ class Agent:
     def remember(self, state, action, reward, next_state, done):
         self.memory.append((state, action, reward, next_state, done))
 
-    def get_action(self, state):
-        self.epsilon = 80 - self.n_games
+    def get_action(self, state, training = True):
+        if training:
+            self.epsilon = 80 - self.n_games
+        else:
+            self.epsilon = -1
         action = [0,0,0]
 
         if random.randint(0,200) < self.epsilon:
@@ -97,7 +101,7 @@ class Agent:
     def train_short_memeory(self, state, action, reward, next_state, done):
         self.trainer.train_step(state, action, reward, next_state, done)
 
-def train():
+def train(episodes):
     plot_scores = []
     plot_mean_scores = []
     total_score = 0
@@ -106,7 +110,7 @@ def train():
     env = SnakeGameAI()
 
     # training loop
-    while True:
+    for i in range(episodes):
         # get agent state
         old_state = agent.get_state(env)
 
@@ -139,6 +143,44 @@ def train():
             plot_mean_scores.append(mean_score)
             plot(plot_scores, plot_mean_scores)
 
+def play(episodes):
+    agent = Agent()
+    env = SnakeGameAI()
+    agent.model.load_state_dict((torch.load('model/model.pth', weights_only=True)))
+    agent.model.eval() 
+    
+    for _ in range(episodes):
+        high_score = 0
+        done = False
+        state = agent.get_state(env)
+        while not done:
+            # get action in inference
+            action = agent.get_action(state, training = False)
+
+            # make action step
+            reward, done, score = env.play_step(action)
+            state = agent.get_state(env)
+        env.reset()
+        high_score = max(score,high_score)
+        print("High Score: ", high_score)
+    
+
+def main():
+    parser = argparse.ArgumentParser(description="Train or play the snake game using a DQN model.")
+    parser.add_argument('--train', action='store_true', help="Train the DQN model.")
+    parser.add_argument('--episodes', type=int, help="Number of training/ inference episodes.", default=100)
+    parser.add_argument('--play', action='store_true', help="Play the game using the trained DQN model.")
+    args = parser.parse_args()
+    
+    if args.train:
+        print("Training mode selected.")
+        train(args.episodes)
+    elif args.play:
+        print("Play mode selected.")
+        play(args.episodes)
+    else:
+        print("Please specify either --train or --play.")
+
 if __name__ == "__main__":
-    train()
+    main()
 
